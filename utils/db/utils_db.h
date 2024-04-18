9b1bf0e756cf1d81ea26cf49b191bbd9b2249e5a
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2024-04-18 17:53:31 -0600

    seed DB

--- utils/db/utils_db.h
+++ utils/db/utils_db.h
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <sqlite3.h>
+
 using namespace std;
 
-int connectToDatabase(sqlite3 **DB)
+int connectToDatabase(sqlite3 **DB, const char *name)
 {
-    int connection = sqlite3_open_v2("Student.db", DB, SQLITE_OPEN_READWRITE, NULL);
+    int connection = sqlite3_open_v2(name, DB, SQLITE_OPEN_READWRITE, NULL);
 
     if (connection)
     {
